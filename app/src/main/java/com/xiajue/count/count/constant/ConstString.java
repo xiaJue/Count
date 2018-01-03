639fdaebf8ac11495c948f38d5b22c8abdc2f1c9
[xiaJue] <[xiajueya@outlook.com]>
2018-01-03 13:12:38 +0800

    add about.htm

---
+++ app/src/main/java/com/xiajue/count/count/constant/ConstString.java
@@ -0,0 +1,8 @@
+package com.xiajue.count.count.constant;
+
+/**
+ * Created by Moing_Admin on 2018/1/3.
+ */
+
+public class ConstString {
+}
---
+++ app/src/main/java/com/xiajue/count/count/ui/AboutActivity.java
@@ -0,0 +1,7 @@
+package com.xiajue.count.count.ui;
+
+/**
+ * Created by Moing_Admin on 2018/1/3.
+ */
+public class AboutActivity {
+}
